1a119094493121034ae1339ec097c5af3bf1ffcc
Suygecu <Suygetcu@mail.ru>
2025-03-21 16:53:40 +0300

    Initial commit

---
+++ src/main/java/com/kostya/taskmanager/controller/UserController.java
@@ -0,0 +1,37 @@
+package com.kostya.taskmanager.controller;
+
+import com.kostya.taskmanager.model.User;
+import com.kostya.taskmanager.repository.UserRepository;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/v1/users")
+public class UserController {
+    private final UserRepository userRepository;
+    private final PasswordEncoder pssswordEncoder = new BCryptPasswordEncoder();
+
+    public UserController(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @GetMapping
+    public List<User> getAllUsers() {
+        return userRepository.findAll();
+    }
+
+    @PostMapping
+    public ResponseEntity<User> createUser(@RequestBody User user) {
+        if (user.getPassword() == null || user.getPassword().isEmpty()) {
+            return ResponseEntity.badRequest().body(user);
+        }
+        user.setPassword(pssswordEncoder.encode(user.getPassword()));
+        User savedUser = userRepository.save(user);
+        return ResponseEntity.ok(savedUser);
+    }
+
+}
---
+++ src/main/java/com/kostya/taskmanager/model/User.java
@@ -0,0 +1,35 @@
+package com.kostya.taskmanager.model;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.*;
+import jakarta.persistence.*;
+
+
+@Entity
+@Table(name = "users")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+
+
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true)
+    private String username;
+
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    @JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
+    @Column(nullable = false)
+    private String password;
+
+    private String role;
+
+}
